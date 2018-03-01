9cbabc37b55f8cf6617271f5b68b215e8eb564bc
Fabian Zoske <git@fzoske.de>
2018-03-01 22:28:38 +0100

    Remove PHP7.0 Bottles

--- Formula/php70-intl.rb
+++ Formula/php70-intl.rb
@@ -4,13 +4,7 @@ class Php70Intl < AbstractPhp70Extension
   init
   desc "Wrapper for the ICU library"
   homepage "https://php.net/manual/en/book.intl.php"
-  revision 24
-
-  bottle do
-    sha256 "e56ea80c08441cc31a51f5a099c5c4a8e8715c26d75238cc3f3d05915ff0fcb8" => :high_sierra
-    sha256 "e371a205612413946e7984f415819e4701550429ff383e2c19f03ce09450de60" => :sierra
-    sha256 "21b82a6b0a3f0d3c73a69999dbd4ae73c8f663f85352a002116b8f000c1d382b" => :el_capitan
-  end
+  revision 25
 
   url PHP_SRC_TARBALL
   sha256 PHP_CHECKSUM[:sha256]
