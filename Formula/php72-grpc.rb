d48cd3834b0aad1fd2e2cd1ce1017b620cbca8d4
Fabian Zoske <git@fzoske.de>
2018-03-01 22:13:56 +0100

    Update php72-grpc to 1.9.0

--- Formula/php72-grpc.rb
+++ Formula/php72-grpc.rb
@@ -4,8 +4,8 @@ class Php72Grpc < AbstractPhp72Extension
   init
   desc "The PHP extension for the gRPC library"
   homepage "https://grpc.io"
-  url "https://pecl.php.net/get/grpc-1.6.0.tgz"
-  sha256 "ded14216247457c04a5e5baf9d3cd44984f4f8042ea4677adfe432ae75c19f09"
+  url "https://pecl.php.net/get/grpc-1.9.0.tgz"
+  sha256 "64edb0ac65b455efa49325116208de1062de0cabf2f9404e9603dcf2471f3df9"
 
   def install
     Dir.chdir "grpc-#{version}"
