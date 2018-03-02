c1498a3b99f9db55fa1b5208b696e6b5b2bb0500
Fabian Zoske <git@fzoske.de>
2018-03-02 17:15:42 +0100

    Update Behat 3.4.3

--- Formula/behat.rb
+++ Formula/behat.rb
@@ -1,19 +1,25 @@
-require File.expand_path("../../Abstract/abstract-php-phar", __FILE__)
+require File.expand_path("../../language/php", __FILE__)
+require File.expand_path("../../Requirements/php-meta-requirement", __FILE__)
 
-class Behat < AbstractPhpPhar
-  init
+class Behat < Formula
+  include Language::PHP::Composer
+
   desc "BDD framework for PHP to help you test business expectations"
   homepage "http://behat.org/"
-  url "https://github.com/Behat/Behat/releases/download/v3.3.0/behat.phar"
-  version "3.3.0"
-  sha256 "d99fdf7e7d9459903ae2f9628e0459ab3357a1c93e0b55ad32f60e625c3cb9f7"
+  url "https://github.com/Behat/Behat/archive/v3.4.3.tar.gz"
+  version "3.4.3"
+  sha256 "c34565c1681e580f2e80a2fcefed0e532bfa76b5701c836a9341cea6a95794d5"
   head "https://github.com/Behat/Behat.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "09478ce163135b6fe50c095d3af4ea54755d4d98f8a28940a98c1334c42b8b4b" => :sierra
-    sha256 "abf26ef96bc570728969dc05e11088cba6c20c68dccc77da55e801ffd4aa2abf" => :el_capitan
-    sha256 "abf26ef96bc570728969dc05e11088cba6c20c68dccc77da55e801ffd4aa2abf" => :yosemite
+  depends_on PhpMetaRequirement
+
+  def install
+    php_modules = Utils.popen_read("php -m")
+    raise "php must be re-compiled, in order to have mbstring support" unless php_modules.include?("mbstring")
+
+    composer_install
+    prefix.install Dir["*"]
+    bin.install_symlink prefix/"bin/envoy"
   end
 
   test do
