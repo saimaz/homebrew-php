2a7415867b230bfe8a061b7bc5a2813b276d6c1c
Fabian Zoske <git@fzoske.de>
2018-03-02 17:24:21 +0100

    terminus 1.7.1

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -6,16 +6,9 @@ class Terminus < Formula
 
   desc "Command-line interface for the Pantheon Platform"
   homepage "https://github.com/pantheon-systems/terminus"
-  url "https://github.com/pantheon-systems/terminus/archive/1.7.0.tar.gz"
-  sha256 "254c3b022bc4931321b61a434f77f62552a177dbd12bc004a83218b17507b022"
+  url "https://github.com/pantheon-systems/terminus/archive/1.7.1.tar.gz"
+  sha256 "d5d870d8f2a654c74f8c391cd8eddd8a0c8d173ab58363cdc7537273c02dd022"
   head "https://github.com/pantheon-systems/terminus.git"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "d10c2e22800455755a3e3ddb30c3cbc2c03d7b726213f6ab616b15b761888939" => :high_sierra
-    sha256 "9e93fad9ac3927b0b977b0387dd346e263ccc38683eadc118a2d220214e4e641" => :sierra
-    sha256 "eb58bfda108430d2a78eb871c10a5ad86a419c8f50eaf43cba3c5680ff1f6262" => :el_capitan
-  end
 
   depends_on PhpMetaRequirement
 
