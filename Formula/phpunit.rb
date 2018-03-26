231800ddbaace7e05da3b209bf79bc5681f19b1a
Fabian Zoske <git@fzoske.de>
2018-03-26 21:09:13 +0200

    phpunit 7.0.3

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -4,8 +4,8 @@ class Phpunit < AbstractPhpPhar
   init
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-7.0.2.phar"
-  sha256 "dd37449ebd5a57d28564b0e56ab82001b86d9bb86abd74d849784dd76e13eeb2"
+  url "https://phar.phpunit.de/phpunit-7.0.3.phar"
+  sha256 "5889db48d1cd666ecd6b8885f47c0bdef1778e81658b5e636e3ada1d2bd382c0"
 
   bottle :unneeded
 
