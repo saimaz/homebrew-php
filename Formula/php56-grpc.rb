98f48b4b505beed23277f7326374df6c22b6307d
Fabian Zoske <git@fzoske.de>
2018-04-10 00:14:34 +0200

    grpc-1.10.1

--- Formula/php56-grpc.rb
+++ Formula/php56-grpc.rb
@@ -4,8 +4,8 @@ class Php56Grpc < AbstractPhp56Extension
   init
   desc "The PHP extension for the gRPC library"
   homepage "https://grpc.io"
-  url "https://pecl.php.net/get/grpc-1.10.0.tgz"
-  sha256 "9abcbb0baebe974a84c6384be9998e510461411cbcee59e22eb17b93fabf66b4"
+  url "https://pecl.php.net/get/grpc-1.10.1.tgz"
+  sha256 "c50e18654ca1ec571429c9ed044370f2af7d2cfb5ca27a39d53dab64d96ab550"
 
   def install
     Dir.chdir "grpc-#{version}"
