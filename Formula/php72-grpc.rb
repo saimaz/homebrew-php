922dd65f1c1193db6f557ea668dabd1338b274cf
Fabian Zoske <git@fzoske.de>
2018-06-13 19:48:49 +0200

    gRPC 1.12.0

--- Formula/php72-grpc.rb
+++ Formula/php72-grpc.rb
@@ -4,8 +4,8 @@ class Php72Grpc < AbstractPhp72Extension
   init
   desc "The PHP extension for the gRPC library"
   homepage "https://grpc.io"
-  url "https://pecl.php.net/get/grpc-1.11.0.tgz"
-  sha256 "b4a04a613b6ec606b0588a2634b8f17a9f0ed8f99852e1e7a863c53631dd5ddb"
+  url "https://pecl.php.net/get/grpc-1.12.0.tgz"
+  sha256 "1f311ec8ec71e98a88ba950ed97671f05d416fe7c44934d8f7ca5946666f98af"
 
   def install
     Dir.chdir "grpc-#{version}"
