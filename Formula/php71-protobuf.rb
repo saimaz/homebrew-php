228f8d41e99d32d4837c4da13a4c03b49185e1f8
Fabian Zoske <git@fzoske.de>
2018-08-16 21:14:24 +0200

    protobuf 3.6.1

--- Formula/php71-protobuf.rb
+++ Formula/php71-protobuf.rb
@@ -4,8 +4,8 @@ class Php71Protobuf < AbstractPhp71Extension
   init
   desc "Google's language-neutral, platform-neutral, extensible mechanism for serializing structured data."
   homepage "https://pecl.php.net/package/protobuf"
-  url "https://pecl.php.net/get/protobuf-3.6.0.tgz"
-  sha256 "8b60841894edef12695a282fb0f8585105b2c70c54a7ef26c7d98d0c093afe6e"
+  url "https://pecl.php.net/get/protobuf-3.6.1.tgz"
+  sha256 "e019fd69e30f998b62bb77ea6241e3d477ed848204bf0561a61144515cc2ee94"
 
   depends_on "protobuf"
 
